7629559cf56b3cd5b005ae7b6ff67af499557c35
Sniuden <safasgasc.asfg@gmail.com>
2023-03-25 16:37:18 +0300

    ORM models

---
+++ Database/connect.py
@@ -0,0 +1,17 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import sessionmaker
+
+from Database.models import create_table
+
+SQLsystem = 'postgresql'
+login = 'postgres'
+password = ''
+host = 'localhost'
+port = '5432'
+db_name = "vkinder_db"
+DSN = f'{SQLsystem}://{login}:{password}@{host}:{port}/{db_name}'
+engine = sq.create_engine(DSN)
+Session = sessionmaker(bind=engine)
+
+with Session() as session:
+    create_table(engine)
